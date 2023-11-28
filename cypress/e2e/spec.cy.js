e9e2d18ffca640ccbd386e7c7710861d479ea749
julianomachads <78459937+julianomachads@users.noreply.github.com>
2023-11-28 15:11:10 -0300

    Ajustes finais do front-end

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,14 +2,12 @@
 
 describe("Novo Produto", () => {
   it("Cadastrar um produto", () => {
-    cy.visit("http://127.0.0.1:5500/");
+    cy.visit("http://localhost:8080/cadastrarProduto");
 
     cy.contains("Novo produto").click();
-    cy.get("#codigo").type("000001");
     cy.get("#descricao").type("Camiseta DACOMP");
-    cy.get("#quantidade").type(20);
-    cy.get("#tipo").select("Produto para revenda");
-    cy.get("#venda").type("50.00");
+    cy.get("#qtd").type(20);
+    cy.get("#valor").type("50.00");
 
     cy.contains("button", "Salvar").click();
   });
--- js/script.js
+++ js/script.js
@@ -12,3 +12,106 @@ const toggleModal = () => {
 [openModalButton, closeModalButton, fade].forEach((el) => {
   el.addEventListener("click", () => toggleModal());
 });
+
+////////////////////////////////////////////////////////////////////////
+
+const filtro = document.querySelector('#buscar');
+
+filtro.addEventListener('input', filtrarProdutos);
+
+function filtrarProdutos(){
+  let listaprods = document.querySelectorAll('#bodyTabela tr');
+  console.log(listaprods);
+  if(filtro.value != ''){
+    console.log("entrou");
+    for(let prod of listaprods){
+      let desc = prod.querySelector('.descricaoProd');
+      desc = desc.textContent.toLowerCase();
+      let filtroDesc = filtro.value.toLowerCase();
+      if(!desc.includes(filtroDesc)){
+        prod.style.display = "none";
+      }else{
+        prod.style.display = "table-row";
+      }
+    }
+  }else{
+    for(let prod of listaprods){
+      prod.style.display = "table-row";
+    }
+  }
+}
+
+//////////
+
+const form = document.getElementById('formCadastro')
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData);
+  const url = new URL("http://localhost:8080/cadastrarProduto")
+
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  }).then(res => res.json()).then(data => {
+    console.log(data);
+    render();
+    form.reset();
+    toggleModal();
+    alert("Produto adicionado com sucesso!");
+  })
+})
+
+//////////
+
+function deletaProduto(id){
+  const url = new URL("http://localhost:8080/deletarProduto/"+id)
+
+  fetch(url, {
+    method: 'DELETE',
+  }).then(res => {
+    console.log(res);
+    render();
+    alert("Produto deletado com sucesso!");
+  })
+}
+
+//////////
+
+const bodyTabela = document.querySelector('#bodyTabela');
+
+function render(){
+  let body = '';
+  const url = new URL("http://localhost:8080/listarProdutos")
+
+  fetch(url)
+  .then((response) => response.json())
+  .then((produtos) => {
+
+    if(produtos.length <= 0){
+      body += '<div>Nenhum produto disponível</div>';
+    }else{
+      produtos.forEach((produto, index) => {
+        body += `
+          <tr >
+            <td>${produto.codProduto}</td>
+            <td class="descricaoProd">${produto.descricao}</td>
+            <td>R$${produto.valor}</td>
+            <td>${produto.qtd}</td>
+            <td class="acoes">
+              <button class="botao excluir" onclick="deletaProduto(${produto.codProduto})">Excluir</button>
+            </td>
+          </tr>
+        `;
+      });
+      bodyTabela.innerHTML = body;
+    }
+  });
+}
+
+render();
+
